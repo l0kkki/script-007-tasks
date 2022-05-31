7f857ca043aa9b72ca02c9a69c757446ea36e561
Roman Palavin <r.palav@yandex.ru>
2022-05-31 13:29:03 +0300

    parsing arguments from cmd

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
+import os
+import argparse
+
+from server import FileService
+
+
 def main():
-    pass
+    default_wd = os.path.dirname(os.path.abspath(__file__))
+    default_wd = os.path.join(default_wd, r'FileServer')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d',
+                        '--directory',
+                        help='Path to working directory',
+                        default=default_wd)
+    cmd_params = parser.parse_args()
+    FileService.change_dir(cmd_params.directory)
 
 
 if __name__ == '__main__':
