b439ca51ed670e98790a5450b12f85419c96e34a
Roman Palavin <r.palav@yandex.ru>
2022-06-02 12:21:29 +0300

    update requriments.txt add conftest using fixture and mark.parametrize

--- server/tests/test_get_file_data.py
+++ server/tests/test_get_file_data.py
@@ -1,15 +1,9 @@
-import os
+import pytest
 
 from server.FileService import get_file_data
 
 
-def test_get_file_data():
-    check_list = ['tmp1', 'tmp2', 'asfafasf']
-    current_folder = os.path.dirname((os.path.abspath(__file__)))
-    for file in check_list:
-        tmp_file = open(file, 'w')
-        tmp_file.close()
-        result = get_file_data(file)
-        if os.path.exists(os.path.join(current_folder, file)):
-            os.remove(os.path.join(current_folder, file))
-        assert isinstance(result, dict) and len(result.keys()) == 5
+@pytest.mark.parametrize('create_single_file', [('tmp1', '111')], indirect=True)
+def test_get_file_data(create_single_file):
+    result = get_file_data(create_single_file)
+    assert isinstance(result, dict) and len(result.keys()) == 5
--- server/tests/test_get_files.py
+++ server/tests/test_get_files.py
@@ -1,16 +1,15 @@
 import os
 
+import pytest
+
 from server.FileService import get_files
 
 
-def test_get_file_data():
-    check_list = ['tmp1', 'tmp2', 'asfafasf']
-    current_folder = os.getcwd()
-    for full_path in [os.path.join(current_folder, file) for file in check_list]:
-        tmp_file = open(full_path, 'w')
-        tmp_file.close()
+@pytest.mark.parametrize('create_list_file', [(
+        ('tmp1', '111'),
+        ('tmp2', '222')
+)], indirect=True)
+def test_get_file_data(create_list_file):
+    os.chdir(create_list_file)
     result = get_files()
-    for full_path in [os.path.join(current_folder, file) for file in check_list]:
-        if os.path.exists(full_path):
-            os.remove(full_path)
     assert isinstance(result, list) and all(isinstance(item, dict) and len(item.keys()) == 4 for item in result)
