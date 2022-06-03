60ff2c8332ebb21df23d10a7965ea1a5da1b948a
Maxim Suslov <suslov.mn@gmail.com>
2022-06-03 09:38:59 +0300

    Add 04_config_solution

--- main.py
+++ main.py
@@ -1,7 +1,59 @@
 #!/usr/bin/env python3
+import logging
+import logging.config
+import sys
+
+import server.FileService as FileService
+from utils.Config import config
+
+
+def setup_logger(level='NOTSET', filename=None):
+    logger_conf = {
+        'version': 1,
+        'formatters': {
+            'default': {
+                'format':
+                    '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+            },
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'default',
+                'level': level,
+            },
+        },
+        'root': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+    if filename:
+        logger_conf['handlers']['file'] = {
+            'class': 'logging.FileHandler',
+            'encoding': 'UTF-8',
+            'formatter': 'default',
+            'filename': filename,
+        }
+        logger_conf['root']['handlers'].append('file')
+    logging.config.dictConfig(logger_conf)
+
+
 def main():
-    pass
+    setup_logger(level=logging.getLevelName(config.log.level.upper()),
+                 filename=config.log.file)
+    logging.debug('started')
+    logging.debug('config %s', config.to_dict())
+
+    FileService.change_dir(config.dir)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(f'\nERROR: Interrupted by user', file=sys.stderr)
+        sys.exit(1)
+    except BaseException as err:
+        print(f'ERROR: Something goes wrong:\n{err}', file=sys.stderr)
+        sys.exit(1)
